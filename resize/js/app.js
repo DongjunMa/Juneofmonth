388fd546dd10ac240e34294cc52e3db24721f992
DongjunMa <79565491+DongjunMa@users.noreply.github.com>
2021-02-24 16:22:59 +0900

    Add files via upload

---
+++ resize/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+   $(window).resize(function(){
+    //브라우저 창 크기가 변경되면
+    let width=$(window).width();
+    //브라우저 창의 가로길이
+    console.log(width);
+    //콘솔에 출력
+   });
+});
